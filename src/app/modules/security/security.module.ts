7dd29f9996df2b9746ed117164bdcd3610148e37
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-02-29 22:56:50 +0530

    modified login components

--- src/app/modules/security/security.module.ts
+++ src/app/modules/security/security.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SecurityRoutingModule } from './security-routing.module';
 import { SecurityComponent } from './security.component';
 import { LoginComponent } from './components/login/login.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -11,9 +12,10 @@ import { LoginComponent } from './components/login/login.component';
     SecurityComponent,
     LoginComponent
   ],
-  imports: [
-    CommonModule,
-    SecurityRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        SecurityRoutingModule,
+        FormsModule
+    ]
 })
 export class SecurityModule { }
