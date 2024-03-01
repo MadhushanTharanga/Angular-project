029d55364be3b82b08468bd51f2eb1886c5c286c
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-03-01 07:12:38 +0530

    modified login components

--- src/app/modules/security/components/login/login.component.ts
+++ src/app/modules/security/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email = '';
+  password = '';
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
+  }
+  login(){
+    if(this.email==='m@gmail.com' && this.password ==='1234'){
+      this.router.navigateByUrl('/console').then();
+    }else {
+      alert('worng input')
+    }
   }
 
 }
