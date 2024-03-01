44b9e3c69670233060b9a4dc8b3245ebab261789
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-03-01 09:22:30 +0530

    modified auth service

--- src/app/modules/security/components/login/login.component.ts
+++ src/app/modules/security/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {CookieService} from "ngx-cookie-service";
+import {AuthService} from "../../../shared/service/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -10,12 +12,16 @@ export class LoginComponent implements OnInit {
 
   email = '';
   password = '';
-  constructor(private router : Router) { }
+  constructor(private router : Router, private authService:AuthService) { }
 
   ngOnInit(): void {
+    if (this.authService.isExistsToken('my-token')){
+      this.router.navigateByUrl('/console').then();
+    }
   }
   login(){
     if(this.email==='m@gmail.com' && this.password ==='1234'){
+      this.authService.createToken(this.email);
       this.router.navigateByUrl('/console').then();
     }else {
       alert('worng input')
