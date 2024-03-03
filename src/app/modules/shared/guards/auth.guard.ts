bb6f43a2ee0ab0d4f30370707ef16f7ed2682700
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-03-03 09:55:30 +0530

    modified guards

--- src/app/modules/shared/guards/auth.guard.ts
+++ src/app/modules/shared/guards/auth.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
+import {AuthService} from "../service/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private routeService : Router, private authService : AuthService) {
+
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
-    return true;
+    if (this.authService.isExistsToken(`my-token`)){
+      return true;
+    }else {
+      this.routeService.navigateByUrl('security/login')
+      return false;
+    }
+
   }
 
 }
